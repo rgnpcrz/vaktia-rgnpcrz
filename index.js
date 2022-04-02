c0eba442c9f5eb24641c5f2475f695842d9918f9
Rigon Paçarizi <48785434+rgnpcrz@users.noreply.github.com>
2022-04-02 17:11:11 +0200

    start

---
+++ index.js
@@ -0,0 +1,33 @@
+var now = new Date().getTime();
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("April 2, 2022 19:10:00").getTime();
+
+
+document.getElementById("iftariSot").innerHTML = "19:10";
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "IFTARI";
+  }
+}, 1000);
