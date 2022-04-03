88d1b34915f622a2564e411ce3a49af5afea516b
rgnpcrz <48785434+rgnpcrz@users.noreply.github.com>
2022-04-03 02:03:46 +0200

    style

--- index.js
+++ index.js
@@ -79,13 +79,13 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = hours + "h "
+  document.getElementById("countDown").innerHTML = hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "IFTARI";
+    document.getElementById("countDown").innerHTML = "IFTARI";
   }
 }, 1000);
 
