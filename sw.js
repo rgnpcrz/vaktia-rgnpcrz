c0163e1e69d50cb5c5d289cf7415b3a0e65f66a6
rgnpcrz <rigonpacharizzi@gmail.com>
2023-04-06 02:22:16 +0200

    trying relative path

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "my-site-cache-v1";
-const urlsToCache = ["/", "/index.html", "/index.js"];
+const urlsToCache = ["/", "/index.html", "/index.js", "/vaktia2023", "/vaktia2023/index.html", "/vaktia2023/index.js"];
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
