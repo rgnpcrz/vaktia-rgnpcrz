d38598e68649216425a0a50580dbb70f2e886022
rgnpcrz <rigonpacharizzi@gmail.com>
2025-02-15 15:44:06 +0100

    days before ramazan starts

--- index2025.js
+++ index2025.js
@@ -121,12 +121,17 @@ var x = setInterval(function () {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(oroq / (1000 * 60 * 60 * 24));
   var hours = Math.floor((oroq % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((oroq % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((oroq % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countDown").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+  if (days > 0) {
+    document.getElementById("countDown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  } else {
+    document.getElementById("countDown").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+  }
 
   // If the count down is finished, write some text
   // if (oroq < 0) {
