25daf26ae999bba372cab2cfe044a713916c75d1
rgnpcrz <rigonpacharizzi@gmail.com>
2025-02-18 22:18:52 +0100

    service worker

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "vaktia_2025_kosove_1_0_0";
+const urlsToCache = ["/", "/index.html", "/styles.css", "/index2025.js", "/img/favicon.png", "/manifest2.json", "https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@500&display=swap"];
+
+// Install the service worker
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch the resources
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+// Activate the service worker
+self.addEventListener("activate", function (event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
