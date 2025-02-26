4020dcb870d475a2de8d31c6d2600765cfaa075c
rgnpcrz <rigonpacharizzi@gmail.com>
2025-02-26 16:05:48 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "vaktia_2025_kosove_3_0_4";
+const CACHE_NAME = "vaktia_2025_kosove_3_0_7";
 const urlsToCache = [
   "/vaktia-rgnpcrz/", // Root path
   "/vaktia-rgnpcrz/index.html",
